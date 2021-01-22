f393b38acb73d1258928638a968976019d20dd14
aberat <aberat@DESKTOP-6M0IKMP>
2021-01-22 16:52:38 +0300

    Add project files.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<Tkeys, Tvalues>
+    {
+        Tkeys[] keysArray;
+        Tvalues[] valuesArray;
+
+        public MyDictionary()
+        {
+            keysArray = new Tkeys[0];
+            valuesArray = new Tvalues[0];
+        }
+        public void Add(Tkeys itemKey, Tvalues itemValue)
+        {
+            Tkeys[] tempKeysArray = keysArray;
+            Tvalues[] tempValuesArray = valuesArray;
+
+            keysArray = new Tkeys[keysArray.Length + 1];
+            valuesArray = new Tvalues[valuesArray.Length + 1];
+            
+            for (int i = 0; i < tempKeysArray.Length; i++)
+            {
+                keysArray[i] = tempKeysArray[i];
+                valuesArray[i] = tempValuesArray[i];
+            }
+            keysArray[keysArray.Length] = itemKey;
+            valuesArray[valuesArray.Length] = itemValue;
+        }
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "ahmet");
+            myDictionary.Add(2, "berat");
+            myDictionary.Add(3, "engin");
+        }
+    }
+}
